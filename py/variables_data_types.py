4a0ac50faf60768184c3d49c2d7ca990af9766d8
BooDotCom <wafiqidraki4332@gmail.com>
2026-04-01 12:25:38 +0800

    commit

--- py/variables_data_types.py
+++ py/variables_data_types.py
@@ -25,10 +25,12 @@ print(x//y) # Floor Division
 print(x%y) # Modulus
 print(x**y) # Exponentiation
 
-#celsius to fahrenheit conversion
+#celsius conversion
 
-celsius = 30
+celsius = int(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
 
 print("Temperature in Celsius:", celsius)
 print("Temperature in Fahrenheit:", fahrenheit)
+print("Temperature in Kelvin:", kelvin)
