88f0977c621d00956439752e0e538c73a416b52b
BooDotCom <wafiqidraki4332@gmail.com>
2026-04-02 10:30:28 +0800

    commit

--- exercise_py/playground.py
+++ exercise_py/playground.py
@@ -1,1 +1,10 @@
-print("hello world")
+#print("hello world")
+
+while True:
+    x = input("Enter the first number: ")
+    try:
+        x = int(x)
+        if type(x) == int:
+            break
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
