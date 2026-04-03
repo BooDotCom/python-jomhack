85e79769bd073535c52013864419d0161c78c42f
BooDotCom <wafiqidraki4332@gmail.com>
2026-04-03 10:32:01 +0800

    commit

--- exercise_py/playground.py
+++ exercise_py/playground.py
@@ -1,10 +1,14 @@
 #print("hello world")
 
-while True:
-    x = input("Enter the first number: ")
-    try:
-        x = int(x)
-        if type(x) == int:
-            break
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
+# while True:
+#     x = input("Enter the first number: ")
+#     try:
+#         x = int(x)
+#         if type(x) == int:
+#             break
+#     except ValueError:
+#         print("Invalid input. Please enter a valid number.")
+
+i = 2
+
+print(int(i**.5))
