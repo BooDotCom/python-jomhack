bf083460c636ad4b0547827b259675416b8829e6
BooDotCom <wafiqidraki4332@gmail.com>
2026-04-03 10:55:19 +0800

    lists

---
+++ py/lists.py
@@ -0,0 +1,27 @@
+food = ["bread", "milk", "eggs", "cheese"]
+numbers = [1, 2, 3, 4, 5]
+rojak = ["PSP", -10, False, 3.142]
+empty = []
+
+# print(food)
+# print(numbers[-2]) #print second last element
+# print(rojak[0:3]) #print from index 0 to 2 (3 is not included)
+# print(food[:2]) #print up to index 1 (2 is not included)
+# print(numbers[2:]) #print after index 1 (2 is included)
+
+#list methods
+
+#mix and match the methods below to manipulate list
+# food.append("butter") #add an element to the end of the list
+# food.insert(6, "ham") #add an element at a specific index
+# food.remove("eggs") #remove an element by value
+# food.pop() #remove the last element
+# food.sort() #sort the list in alphabetical/numerical order
+# food.reverse() #reverse the order of the list
+
+# food + ["ham", "butter"] #concatenate two lists
+# food * 2 #repeat the list twice
+# "milk" in food #check if an element is in the list. Is Boolean
+# len(food) #get the number of elements in the list
+
+print(food)
