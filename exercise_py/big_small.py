56992d7e544b61715a466d4ddcc17a419a18503a
BooDotCom <wafiqidraki4332@gmail.com>
2026-04-03 10:59:15 +0800

    largest_smallest

---
+++ exercise_py/big_small.py
@@ -0,0 +1,11 @@
+num_list = [-10, 0, 5, 3.14, 100, -2.718]
+
+#find the biggest and smallest number in num_list
+biggest = max(num_list)
+smallest = min(num_list)
+
+#max(): returns the largest item in an iterable or the largest of two or more arguments.
+#min(): returns the smallest item in an iterable or the smallest of two or more arguments.  
+
+print("The biggest number is: ", biggest)
+print("The smallest number is: ", smallest)
